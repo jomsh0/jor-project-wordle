9a42d1766d92b20f5cd4f3ba59a7de80633fb0b1
John Sherrell <dev@ohn.sh>
2025-12-10 11:53:32 -0600

    Update dependencies; update bundling to work with latest parcel.

--- dist/App.js
+++ dist/App.js
@@ -8,10 +8,10 @@ import $kyR4X$react from "react";
 const $4c225ee090a8f350$export$4812e460280c6ef2 = (arr)=>{
     return arr[Math.floor(Math.random() * arr.length)];
 };
-const $4c225ee090a8f350$export$4f5d2d50c9deca37 = (...tokens)=>tokens.filter((token)=>token && typeof token === "string").join(" ");
+const $4c225ee090a8f350$export$4f5d2d50c9deca37 = (...tokens)=>tokens.filter((token)=>token && typeof token === 'string').join(' ');
 const $4c225ee090a8f350$export$d02631cccf789723 = (start, end, step = 1)=>{
     let output = [];
-    if (typeof end === "undefined") {
+    if (typeof end === 'undefined') {
         end = start;
         start = 0;
     }
@@ -21,56 +21,56 @@ const $4c225ee090a8f350$export$d02631cccf789723 = (start, end, step = 1)=>{
 
 
 const $703da7409b6b84ac$export$aa71b5c75a504bc0 = [
-    "AGENT",
-    "WORLD",
-    "ABOUT",
-    "HEART",
-    "WATER",
-    "SIXTY",
-    "BOARD",
-    "MONTH",
-    "MUSIC",
-    "PARTY",
-    "PIANO",
-    "MOUTH",
-    "WOMAN",
-    "SUGAR",
-    "AMBER",
-    "DREAM",
-    "LAUGH",
-    "TIGER",
-    "EARTH",
-    "MONEY",
-    "WORDS",
-    "SMILE",
-    "LEMON",
-    "SOUTH",
-    "AFTER",
-    "STONE",
-    "THING",
-    "LIGHT",
-    "STORY",
-    "POWER",
-    "TODAY",
-    "RANGE",
-    "PEARL",
-    "VENOM",
-    "PROXY",
-    "ROUND",
-    "HOVER",
-    "CANDY",
-    "ABOVE",
-    "PHONE",
-    "OTHER",
-    "SMART",
-    "BLACK",
-    "MAGIC",
-    "FRUIT",
-    "RADIO",
-    "ROYAL",
-    "HONEY",
-    "FLAKE",
-    "SOUND"
+    'AGENT',
+    'WORLD',
+    'ABOUT',
+    'HEART',
+    'WATER',
+    'SIXTY',
+    'BOARD',
+    'MONTH',
+    'MUSIC',
+    'PARTY',
+    'PIANO',
+    'MOUTH',
+    'WOMAN',
+    'SUGAR',
+    'AMBER',
+    'DREAM',
+    'LAUGH',
+    'TIGER',
+    'EARTH',
+    'MONEY',
+    'WORDS',
+    'SMILE',
+    'LEMON',
+    'SOUTH',
+    'AFTER',
+    'STONE',
+    'THING',
+    'LIGHT',
+    'STORY',
+    'POWER',
+    'TODAY',
+    'RANGE',
+    'PEARL',
+    'VENOM',
+    'PROXY',
+    'ROUND',
+    'HOVER',
+    'CANDY',
+    'ABOVE',
+    'PHONE',
+    'OTHER',
+    'SMART',
+    'BLACK',
+    'MAGIC',
+    'FRUIT',
+    'RADIO',
+    'ROYAL',
+    'HONEY',
+    'FLAKE',
+    'SOUND'
 ];
 
 
@@ -80,13 +80,13 @@ const $cc3efe53df664a72$export$5c6373d26cb292a9 = 6;
 
 
 function $c25ecea5d7a90986$export$2e2bcd8739ae039({ onGuess: onGuess, ...delegated }) {
-    const [guess, setGuess] = (0, $kyR4X$react).useState("");
+    const [guess, setGuess] = (0, $kyR4X$react).useState('');
     return /*#__PURE__*/ (0, $kyR4X$jsxs)("form", {
         className: "guess-input-wrapper",
         onSubmit: (event)=>{
             event.preventDefault();
             onGuess(guess);
-            setGuess("");
+            setGuess('');
         },
         children: [
             /*#__PURE__*/ (0, $kyR4X$jsx)("label", {
@@ -118,14 +118,14 @@ function $c25ecea5d7a90986$export$2e2bcd8739ae039({ onGuess: onGuess, ...delegat
     // dealt with this character (it's correct, or misplaced).
     const SOLVED_CHAR = "\u2713";
     if (!guess) return null;
-    const guessChars = guess.toUpperCase().split("");
-    const answerChars = answer.split("");
+    const guessChars = guess.toUpperCase().split('');
+    const answerChars = answer.split('');
     const result = [];
     // Step 1: Look for correct letters.
     for(let i = 0; i < guessChars.length; i++)if (guessChars[i] === answerChars[i]) {
         result[i] = {
             letter: guessChars[i],
-            status: "correct"
+            status: 'correct'
         };
         answerChars[i] = SOLVED_CHAR;
         guessChars[i] = SOLVED_CHAR;
@@ -134,10 +134,10 @@ function $c25ecea5d7a90986$export$2e2bcd8739ae039({ onGuess: onGuess, ...delegat
     // it must be incorrect.
     for(let i = 0; i < guessChars.length; i++){
         if (guessChars[i] === SOLVED_CHAR) continue;
-        let status = "incorrect";
+        let status = 'incorrect';
         const misplacedIndex = answerChars.findIndex((char)=>char === guessChars[i]);
         if (misplacedIndex >= 0) {
-            status = "misplaced";
+            status = 'misplaced';
             answerChars[misplacedIndex] = SOLVED_CHAR;
         }
         result[i] = {
@@ -151,11 +151,11 @@ function $c25ecea5d7a90986$export$2e2bcd8739ae039({ onGuess: onGuess, ...delegat
 
 
 function $486aae2b83203c5c$export$2e2bcd8739ae039({ answer: answer, children: children }) {
-    const boxes = typeof children === "string" ? (0, $4323e23a5c635aef$export$6e90f449c7e09a62)(children, answer) : new Array(5).fill({});
+    const boxes = typeof children === 'string' ? (0, $4323e23a5c635aef$export$6e90f449c7e09a62)(children, answer) : new Array(5).fill({});
     return /*#__PURE__*/ (0, $kyR4X$jsx)("p", {
         className: "guess",
-        children: boxes.map(({ letter: letter = "", status: status } = {}, index)=>/*#__PURE__*/ (0, $kyR4X$jsx)("span", {
-                className: (0, $4c225ee090a8f350$export$4f5d2d50c9deca37)("cell", status),
+        children: boxes.map(({ letter: letter = '', status: status } = {}, index)=>/*#__PURE__*/ (0, $kyR4X$jsx)("span", {
+                className: (0, $4c225ee090a8f350$export$4f5d2d50c9deca37)('cell', status),
                 children: letter
             }, index))
     });
@@ -192,16 +192,16 @@ function $40e426ba79875e01$var$Game() {
     return /*#__PURE__*/ (0, $kyR4X$jsxs)("div", {
         className: "game-wrapper",
         children: [
-            status === "won" && /*#__PURE__*/ (0, $kyR4X$jsx)($40e426ba79875e01$var$HappyBanner, {
+            status === 'won' && /*#__PURE__*/ (0, $kyR4X$jsx)($40e426ba79875e01$var$HappyBanner, {
                 numGuesses: guesses.length
             }),
-            status === "lost" && /*#__PURE__*/ (0, $kyR4X$jsx)($40e426ba79875e01$var$SadBanner, {}),
+            status === 'lost' && /*#__PURE__*/ (0, $kyR4X$jsx)($40e426ba79875e01$var$SadBanner, {}),
             /*#__PURE__*/ (0, $kyR4X$jsx)((0, $5376c52ebd29c342$export$2e2bcd8739ae039), {
                 guesses: guesses,
                 answer: $40e426ba79875e01$var$answer
             }),
             /*#__PURE__*/ (0, $kyR4X$jsx)((0, $c25ecea5d7a90986$export$2e2bcd8739ae039), {
-                disabled: status !== "active",
+                disabled: status !== 'active',
                 onGuess: (guess)=>setGuesses([
                         ...guesses,
                         guess
@@ -211,9 +211,9 @@ function $40e426ba79875e01$var$Game() {
     });
 }
 function $40e426ba79875e01$var$getStatus(guesses) {
-    if (guesses.includes($40e426ba79875e01$var$answer)) return "won";
-    else if (guesses.length >= (0, $cc3efe53df664a72$export$5c6373d26cb292a9)) return "lost";
-    else return "active";
+    if (guesses.includes($40e426ba79875e01$var$answer)) return 'won';
+    else if (guesses.length >= (0, $cc3efe53df664a72$export$5c6373d26cb292a9)) return 'lost';
+    else return 'active';
 }
 const $40e426ba79875e01$var$HappyBanner = ({ numGuesses: numGuesses })=>/*#__PURE__*/ (0, $kyR4X$jsx)("div", {
         className: "happy banner",
@@ -223,12 +223,12 @@ const $40e426ba79875e01$var$HappyBanner = ({ numGuesses: numGuesses })=>/*#__PUR
                     children: "Congratulations!"
                 }),
                 " Got it in",
-                " ",
+                ' ',
                 /*#__PURE__*/ (0, $kyR4X$jsxs)("strong", {
                     children: [
                         numGuesses,
                         " ",
-                        numGuesses === 1 ? "guess" : "guesses"
+                        numGuesses === 1 ? 'guess' : 'guesses'
                     ]
                 }),
                 "."
